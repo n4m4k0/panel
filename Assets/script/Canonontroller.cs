70f631e9a3a126e5cb5ff2d6b81822c06a1f4c20
408-07\student <FCA22090022@edu.fca.ac.jp>
2024-06-25 19:30:59 +0900

    難易度変更追加

--- Assets/script/Canonontroller.cs
+++ Assets/script/Canonontroller.cs
@@ -1,7 +1,8 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using System;
+using Cysharp.Threading.Tasks;
+
 /// <summary>
 /// 弾を射出するスクリプト
 /// </summary>
@@ -14,58 +15,76 @@ public class Canonontroller : MonoBehaviour
     int canonnspeed=1000;
     //random
     List<int> randomTime = new List<int>();
+
+    bool isCreate = false; // 生成フラグ
+    float nowCreateIntarval;
+
+    const float LV1Interval = 5.0f;
+    const float LV2Interval = 3.0f;
+    const float LV3Interval = 1.0f;
+
     private void Start()
     {
-        randomTime.Add(1800);
-        randomTime.Add(3600);
-        randomTime.Add(7200);
-    }
 
-    int count;
+        nowCreateIntarval = LV1Interval;
+        // 生成インターバル開始
+        StartCreateInterval();
+    }
     private int index;
     // Update is called once per frame
     void Update()
     {
-        //
-        int rand = (int)Random.Range(1,10000);
+        if (isCreate)
+        {
+            CreateShell(); // 球生成
 
+            isCreate = false; // 生成フラグを下す
+            // 生成インターバル開始
+            StartCreateInterval();
+        }
+    }
+
+    async void StartCreateInterval()
+    {
+        // CreateIntarval秒待機後生成フラグ切り替え
+        await UniTask.Delay(TimeSpan.FromSeconds(nowCreateIntarval));
+        isCreate = true;
+    }
+
+    void CreateShell()
+    {
         for (int i = 0; i < canonn.Count; i++)
         {
-            if (rand  <= 5)
+            GameObject shell = Instantiate(canonnball[UnityEngine.Random.Range(0, 3)], canonn[i].transform.position, Quaternion.identity);
+            Rigidbody shellRb = shell.GetComponent<Rigidbody>();
+            //各々の位置から飛ばす
+            switch (i)
             {
-                    GameObject shell = Instantiate(canonnball[Random.Range(0,3)], canonn[i].transform.position, Quaternion.identity);
-                    Rigidbody shellRb = shell.GetComponent<Rigidbody>();
-                //各々の位置から飛ばす
-                switch(i)
-                {
+                case 0:
+                    // 弾速は自由に設定
+                    shellRb.AddForce(transform.forward * canonnspeed);
 
-                    case 0:
-                            // 弾速は自由に設定
-                            shellRb.AddForce(transform.forward * canonnspeed);
-                        
-                        break;
-                     case 1:
-                            shellRb.AddForce(transform.right * -canonnspeed);
-                        
-                        break;
-                    case 2:
-                            shellRb.AddForce(transform.forward * -canonnspeed);
-                        
-                        break;
+                    break;
+                case 1:
+                    shellRb.AddForce(transform.right * -canonnspeed);
 
-                    case 3:
-                            shellRb.AddForce(transform.right * canonnspeed);
-                        
-                        break;
- 
-                    default:
-                        break;
-                }
+                    break;
+                case 2:
+                    shellRb.AddForce(transform.forward * -canonnspeed);
 
-                // ５秒後に砲弾を破壊する
-                Destroy(shell, 5.0f);
-                index = (int)Random.Range(0, 2);
+                    break;
+
+                case 3:
+                    shellRb.AddForce(transform.right * canonnspeed);
+
+                    break;
+
+                default:
+                    break;
             }
-        }count += 1;
+            // ５秒後に砲弾を破壊する
+            Destroy(shell, 5.0f);
+            index = (int)UnityEngine.Random.Range(0, 2);
+        }
     }
 }
